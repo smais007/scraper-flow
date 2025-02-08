3e6a6d4234317591769d5f3cc71250c23d3d2e5e
smais007 <smaisshawon@gmail.com>
2025-02-08 23:55:34 +0600

    wip: home layout

---
+++ components/global/DesktopSidebar.tsx
@@ -0,0 +1,31 @@
+import { HomeIcon, Layers2Icon } from "lucide-react";
+import React from "react";
+
+const routes = [
+  {
+    href: "",
+    label: "Home",
+    icon: HomeIcon,
+  },
+  {
+    href: "workflows",
+    label: "Workflows",
+    icon: Layers2Icon,
+  },
+  {
+    href: "",
+    label: "Home",
+    icon: HomeIcon,
+  },
+  {
+    href: "",
+    label: "Home",
+    icon: HomeIcon,
+  },
+];
+
+const DesktopSidebar = () => {
+  return <div>DesktopSidebar</div>;
+};
+
+export default DesktopSidebar;
